41dd6a9b41367fd450f9085d83efe221ac2b1f9f
Mahdi <mahdibagherichanel@gmail.com>
2026-01-11 06:53:42 +0330

    add children for layout

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -1,42 +1,14 @@
-import TabelData from "@/core/components/custom/blocks/tabel/TabelData";
-import Modal from "@/core/components/custom/ui/Modal";
-import { TypographyH3 } from "@/core/components/custom/ui/Typography";
-import { Button } from "@/core/components/shadcn/ui/button";
 import HeaderAdmin from "@/core/features/admin/components/blocks/header/HeaderAdmin";
 import SidebarAdmin from "@/core/features/admin/components/blocks/sidebar/SidebarAdmin";
-import FormCreateProduct from "@/core/features/admin/components/ui/product/FormCreateProduct";
-import { Plus } from "lucide-react";
+import React from "react";
 
-function layout() {
+function layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex gap-1">
-      <section className="bg-background w-2/10 h-screen p-4">
-        <SidebarAdmin />
-      </section>
+      <SidebarAdmin />
       <section className="flex-col w-full">
         <HeaderAdmin />
-        <div className="p-4">
-          <div className="flex items-center justify-between">
-            <TypographyH3>All Products</TypographyH3>
-            <Modal
-              trigger={
-                <Button variant="outline">
-                  <Plus /> ADD NEW PRODUCT
-                </Button>
-              }
-              title="new product"
-              description="Please enter product information"
-              className="w-2xl"
-            >
-              <FormCreateProduct />
-            </Modal>
-          </div>
-          <TabelData
-            CheckboxTabel={true}
-            ShowImage={true}
-            TableTextHead={["Product", "Color", "Category", "Price"]}
-          />
-        </div>
+        {children}
       </section>
     </main>
   );
--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { filterTabel } from "@/core/api-route/admin/handlers/tabel/filterTabel";
+import Modal from "@/core/components/custom/ui/Modal";
+import { TypographyH3 } from "@/core/components/custom/ui/Typography";
+import { Button } from "@/core/components/shadcn/ui/button";
+import FormCreateProduct from "@/core/features/admin/components/ui/product/FormCreateProduct";
+import TabelProducts from "@/core/features/admin/components/ui/product/TabelProducts";
+import { categoryP } from "@prisma/client";
+import { Plus } from "lucide-react";
 
-function page() {
-  return <div>page</div>;
+async function page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const params = await searchParams;
+
+  const Pdata = await filterTabel({
+    category: params.category as categoryP,
+    limit: params.limit ? Number(params.limit) : 5,
+    page: params.page ? Number(params.page) : 1,
+    search: params.search ?? "",
+    sortOrder: (params.sortOrder as "desc" | "asc") ?? "asc",
+  });
+
+  console.log(Pdata);
+  return (
+    <section className="p-4">
+      <div className="flex items-center justify-between">
+        <TypographyH3>All Products</TypographyH3>
+
+        <Modal
+          trigger={
+            <Button variant="outline">
+              <Plus /> ADD NEW PRODUCT
+            </Button>
+          }
+          title="new product"
+          description="Please enter product information"
+          className="w-2xl"
+        >
+          <FormCreateProduct />
+        </Modal>
+      </div>
+      <TabelProducts />
+    </section>
+  );
 }
 
 export default page;
