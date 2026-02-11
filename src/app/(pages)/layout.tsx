0652874205f4d2510228e1ae4f0692c71355c754
Mahdi <mahdibagherichanel@gmail.com>
2026-02-11 22:23:07 +0330

    add layput paneluser

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,15 +1,17 @@
 import Footer from "@/core/features/main/components/blocks/footer/Footer";
 import Header from "@/core/features/main/components/blocks/header/Header";
+import { isLogin } from "@/core/utils/isLogin";
 import React from "react";
 
-function layout({
+async function layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isSignUp = await isLogin();
   return (
     <main>
-      <Header />
+      <Header isSignUp={isSignUp} />
       {children}
       <Footer />
     </main>
