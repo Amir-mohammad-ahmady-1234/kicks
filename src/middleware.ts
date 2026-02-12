2cca99e55b2b91de59d5a1ca093a3b412f469ec1
Mahdi <mahdibagherichanel@gmail.com>
2026-02-12 20:02:36 +0330

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const isProtectedPath =
+    pathname.startsWith("/userpanel") || pathname.startsWith("/admin");
+
+  if (!isProtectedPath) {
+    return NextResponse.next();
+  }
+  const licenseCookie = request.cookies.get("license")?.value;
+
+  if (!licenseCookie) {
+    return NextResponse.redirect(new URL("/auth", request.url));
+  }
+  return NextResponse.next();
+}
+export const config = {
+  matcher: ["/userpanel/:path*", "/admin/:path*"],
+};
