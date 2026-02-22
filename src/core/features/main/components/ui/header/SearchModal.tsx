bd8d89453527811f5518fa908a8eea316e7c194d
Mahdi <mahdibagherichanel@gmail.com>
2026-02-22 10:04:51 +0330

    fix bug for vercerl error in deployment

--- src/core/features/main/components/ui/header/SearchModal.tsx
+++ src/core/features/main/components/ui/header/SearchModal.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Modal from "@/core/components/custom/ui/Modal";
-import { ModeToggle } from "@/core/components/custom/ui/ModeToggle";
 import { Button } from "@/core/components/shadcn/ui/button";
 import { Input } from "@/core/components/shadcn/ui/input";
 import { ArrowRight, Search } from "lucide-react";
